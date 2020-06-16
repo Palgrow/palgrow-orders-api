1f25bab40ee922fd3ad462142866bf1d49d0ef29
Ndu <yomi.ndu@icloud.com>
2020-06-17 00:41:52 +0100

    chore: update model config

--- application/config/models.js
+++ application/config/models.js
@@ -69,9 +69,10 @@ module.exports.models = {
   ***************************************************************************/
 
   attributes: {
+    id: { type: 'string', columnName: '_id' },
     created_at: { type: 'number', autoCreatedAt: true, },
     updated_at: { type: 'number', autoUpdatedAt: true, },
-    id: { type: 'number', autoIncrement: true, },
+    // id: { type: 'number', autoIncrement: true, },
     //--------------------------------------------------------------------------
     //  /\   Using MongoDB?
     //  ||   Replace `id` above with this instead:
